efd0833d27bb0b17a1431a7ed9b4fe51d0a12c80
miguel <migueldelafuente4@gmail.com>
2023-06-29 11:53:24 +0200

    rm non DD in uncoupled H22

--- src/module_exportings.f90
+++ src/module_exportings.f90
@@ -1505,64 +1505,64 @@ do qq1 = 1, VSsp_dim
 !! Loop for the HO basis, getting the
 !!---------------------------------------------------------------------------
 !! Loop for the Hamiltonian, using tr and the permutations on the m.e.
-do kk = 1, hamil_H2dim
-  i1 = hamil_abcd(1+4*(kk-1))
-  i2 = hamil_abcd(2+4*(kk-1))
-  i3 = hamil_abcd(3+4*(kk-1))
-  i4 = hamil_abcd(4+4*(kk-1))
-  h2b  = hamil_H2(kk)
-  perm = hamil_trperm(kk)
-
-  aux = zero
-  !!! Loop on time reversal
-  do it = 1, 2
-    if ( it == 2 ) then
-      if ( HOsp_2mj(i1) + HOsp_2mj(i2) == 0 ) cycle
-      call find_timerev(perm,i1,i2,i3,i4)
-      h2b = sign(one,perm*one) * h2b
-    endif
-
-     aux = aux + (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i1,i2,i3,i4))
-     aux = aux - (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i1,i2,i4,i3))
-     aux = aux - (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i2,i1,i3,i4))
-     aux = aux + (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i2,i1,i4,i3))
-
-     if ((kdelta(i1,i3) * kdelta(i2,i4)) .EQ. 1) cycle
-
-     aux = aux + (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i3,i4,i1,i2))
-     aux = aux - (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i3,i4,i2,i1))
-     aux = aux - (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i4,i3,i1,i2))
-     aux = aux + (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i4,i3,i2,i1))
-  enddo
-
-  !! add the result to the uncoupled quasi particle matrix element
-  uncoupled_H22_VS(qq1,qq2,qq3,qq4) = uncoupled_H22_VS(qq1,qq2,qq3,qq4) + aux
-
-end do
-
-!! Loop for the Density Dependent term
-!! (does not use TR and perm. sort but explicit separation on the isospin)
-!do kk = 1, hamil_DD_H2dim
-!
-!  i1 = hamil_DD_abcd(1+4*(kk-1))
-!  i2 = hamil_DD_abcd(2+4*(kk-1))
-!  i3 = hamil_DD_abcd(3+4*(kk-1))
-!  i4 = hamil_DD_abcd(4+4*(kk-1))
+!do kk = 1, hamil_H2dim
+!  i1 = hamil_abcd(1+4*(kk-1))
+!  i2 = hamil_abcd(2+4*(kk-1))
+!  i3 = hamil_abcd(3+4*(kk-1))
+!  i4 = hamil_abcd(4+4*(kk-1))
+!  h2b  = hamil_H2(kk)
+!  perm = hamil_trperm(kk)
 !
 !  aux = zero
-!  aux = aux+ (hamil_DD_H2_byT(1,kk) * &
-!              bogo_UV_operations_for_H22(q1,q2,q3,q4, i1,i2,i3,i4))
-!  aux = aux+ (hamil_DD_H2_byT(2,kk) * &
-!              bogo_UV_operations_for_H22(q1,q2,q3,q4, i1,i2+sn,i3,i4+sn))
-!  aux = aux+ (hamil_DD_H2_byT(3,kk) * &
-!              bogo_UV_operations_for_H22(q1,q2,q3,q4, i1,i2+sn,i3+sn,i4))
-!  aux = aux+ (hamil_DD_H2_byT(4,kk) * &
-!              bogo_UV_operations_for_H22(q1,q2,q3,q4, i1+sn,i2+sn,i3+sn,i4+sn))
+!  !!! Loop on time reversal
+!  do it = 1, 2
+!    if ( it == 2 ) then
+!      if ( HOsp_2mj(i1) + HOsp_2mj(i2) == 0 ) cycle
+!      call find_timerev(perm,i1,i2,i3,i4)
+!      h2b = sign(one,perm*one) * h2b
+!    endif
+!
+!     aux = aux + (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i1,i2,i3,i4))
+!     aux = aux - (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i1,i2,i4,i3))
+!     aux = aux - (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i2,i1,i3,i4))
+!     aux = aux + (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i2,i1,i4,i3))
+!
+!     if ((kdelta(i1,i3) * kdelta(i2,i4)) .EQ. 1) cycle
+!
+!     aux = aux + (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i3,i4,i1,i2))
+!     aux = aux - (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i3,i4,i2,i1))
+!     aux = aux - (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i4,i3,i1,i2))
+!     aux = aux + (h2b * bogo_UV_operations_for_H22(q1,q2,q3,q4, i4,i3,i2,i1))
+!  enddo
 !
 !  !! add the result to the uncoupled quasi particle matrix element
 !  uncoupled_H22_VS(qq1,qq2,qq3,qq4) = uncoupled_H22_VS(qq1,qq2,qq3,qq4) + aux
 !
 !end do
+
+!! Loop for the Density Dependent term
+!! (does not use TR and perm. sort but explicit separation on the isospin)
+do kk = 1, hamil_DD_H2dim
+
+  i1 = hamil_DD_abcd(1+4*(kk-1))
+  i2 = hamil_DD_abcd(2+4*(kk-1))
+  i3 = hamil_DD_abcd(3+4*(kk-1))
+  i4 = hamil_DD_abcd(4+4*(kk-1))
+
+  aux = zero
+  aux = aux+ (hamil_DD_H2_byT(1,kk) * &
+              bogo_UV_operations_for_H22(q1,q2,q3,q4, i1,i2,i3,i4))
+  aux = aux+ (hamil_DD_H2_byT(2,kk) * &
+              bogo_UV_operations_for_H22(q1,q2,q3,q4, i1,i2+sn,i3,i4+sn))
+  aux = aux+ (hamil_DD_H2_byT(3,kk) * &
+              bogo_UV_operations_for_H22(q1,q2,q3,q4, i1,i2+sn,i3+sn,i4))
+  aux = aux+ (hamil_DD_H2_byT(4,kk) * &
+              bogo_UV_operations_for_H22(q1,q2,q3,q4, i1+sn,i2+sn,i3+sn,i4+sn))
+
+  !! add the result to the uncoupled quasi particle matrix element
+  uncoupled_H22_VS(qq1,qq2,qq3,qq4) = uncoupled_H22_VS(qq1,qq2,qq3,qq4) + aux
+
+end do
 !!---------------------------------------------------------------------------
 
 
