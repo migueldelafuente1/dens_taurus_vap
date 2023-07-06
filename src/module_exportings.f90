8711f6510379c860ff68941a181ea2381ed53d6c
migueldelafuente1 <migueldelafuente4@gmail.com>
2023-07-06 18:49:25 +0200

    Fixing UV operation that lead to a posible bug in the PN space H qp 22

--- src/module_exportings.f90
+++ src/module_exportings.f90
@@ -1443,9 +1443,13 @@ allocate(uncoupled_H22_VS(VSsp_dim,VSsp_dim,VSsp_dim,VSsp_dim))
 uncoupled_H22_VS = zero
 
 !! Apply the transformation on the U and V
-call dgemm('n','n', ndim, ndim, ndim, one, bogo_U0, ndim, transf_H11, ndim,&
+!call dgemm('n','n', ndim, ndim, ndim, one, bogo_U0, ndim, transf_H11, ndim,&
+!           zero, U_trans, ndim)
+!call dgemm('n','n', ndim, ndim, ndim, one, bogo_V0, ndim, transf_H11, ndim,&
+!           zero, V_trans, ndim)
+call dgemm('n','n', ndim, ndim, ndim, one, transf_H11, ndim, bogo_U0, ndim,&
            zero, U_trans, ndim)
-call dgemm('n','n', ndim, ndim, ndim, one, bogo_V0, ndim, transf_H11, ndim,&
+call dgemm('n','n', ndim, ndim, ndim, one, transf_H11, ndim, bogo_V0, ndim,&
            zero, V_trans, ndim)
 
 !! Re-import the matrix elements of the non-DD hamiltonian from the .red file
