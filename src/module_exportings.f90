7db2a0a85e385f4aec1a1b836dd22a313b534392
migueldelafuente1 <migueldelafuente4@gmail.com>
2023-07-13 16:17:08 +0200

    introducing test for H22 VS antisymmetry

--- src/module_exportings.f90
+++ src/module_exportings.f90
@@ -1452,9 +1452,9 @@ uncoupled_H22_VS = zero
 !call dgemm('n','n', ndim, ndim, ndim, one, bogo_V0, ndim, transf_H11, ndim,&
 !           zero, V_trans, ndim)
 
-call dgemm('t','t', ndim, ndim, ndim, one, transf_H11, ndim, bogo_U0, ndim,&
+call dgemm('n','t', ndim, ndim, ndim, one, transf_H11, ndim, bogo_U0, ndim,&
            zero, U_trans, ndim)
-call dgemm('t','t', ndim, ndim, ndim, one, transf_H11, ndim, bogo_V0, ndim,&
+call dgemm('n','t', ndim, ndim, ndim, one, transf_H11, ndim, bogo_V0, ndim,&
            zero, V_trans, ndim)
 
 do i1 = 1, ndim !transpose
