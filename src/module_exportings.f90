29a2efd91007a42e62e4d84fdbef43b6a384830d
migueldelafuente1 <migueldelafuente4@gmail.com>
2023-07-24 21:11:19 +0200

    switch off BB, FULL HAMIL = FALSE

--- src/module_exportings.f90
+++ src/module_exportings.f90
@@ -2046,7 +2046,7 @@ temp_indx_perm(it,1) = i1
 temp_indx_perm(it,2) = i2
 temp_indx_perm(it,3) = i3
 temp_indx_perm(it,4) = i4
-temp_h2b_perm (it)   = test_hamil_bb(i1,i2, i3,i4)
+temp_h2b_perm (it)   = test_hamil_bb(i1,i2, i3,i4) * 0.0d0
 
 aux_step_h2(it,1,1) = bogo_UV_operations_for_H22(q1,q2,q3,q4, i1,i2,i3,i4)
 aux_step_h2(it,1,2) = aux_step_h2(it,1,1) * temp_h2b_perm (it)
@@ -2148,7 +2148,7 @@ do kk = 1, hamil_H2dim
   i2 = hamil_abcd(2+4*(kk-1))
   i3 = hamil_abcd(3+4*(kk-1))
   i4 = hamil_abcd(4+4*(kk-1))
-  h2b  = hamil_H2(kk)          !! all are > TOL.
+  h2b  = hamil_H2(kk)     0.0d0     !! all are > TOL.
   perm = hamil_trperm(kk)
 
   temp_indx_perm = 0
