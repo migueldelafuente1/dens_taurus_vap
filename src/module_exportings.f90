42f56bf9cc6c9ff3f8c1b2f17b7e6c9bd60bf74d
migueldelafuente1 <migueldelafuente4@gmail.com>
2023-07-25 11:59:19 +0200

    turn ON explicit hamiltonian, test NO DD

--- src/module_exportings.f90
+++ src/module_exportings.f90
@@ -1734,7 +1734,7 @@ do i1 = 1, ndim
     do i3 = 1, ndim
       do i4 = 1, ndim
 
-aux = test_hamil_bb(i1,i2, i3,i4) * 0.0d0
+aux = test_hamil_bb(i1,i2, i3,i4)
 aux = aux * bogo_UV_operations_for_H22(q1,q2,q3,q4, i1,i2,i3,i4)
 uncoupled_H22_VS(qq1,qq2,qq3,qq4) = uncoupled_H22_VS(qq1,qq2,qq3,qq4) + aux
 
@@ -1751,7 +1751,7 @@ do i1 = 1, ndim
     do i3 = 1, ndim
       do i4 = 1, ndim
 
-aux = test_hamil_dd(i1,i2, i3,i4)
+aux = test_hamil_dd(i1,i2, i3,i4) * 0.0d0
 aux = aux * bogo_UV_operations_for_H22(q1,q2,q3,q4, i1,i2,i3,i4)
 uncoupled_H22_VS(qq1,qq2,qq3,qq4) = uncoupled_H22_VS(qq1,qq2,qq3,qq4) + aux
 
@@ -1775,7 +1775,7 @@ do kk = 1, hamil_H2dim
   i2 = hamil_abcd(2+4*(kk-1))
   i3 = hamil_abcd(3+4*(kk-1))
   i4 = hamil_abcd(4+4*(kk-1))
-  h2b  = hamil_H2(kk) * 0.0d0
+  h2b  = hamil_H2(kk)
   perm = hamil_trperm(kk)
 
   aux = zero
@@ -1819,6 +1819,7 @@ do kk = 1, hamil_DD_H2dim
   i4 = hamil_DD_abcd(4+4*(kk-1))
 
   aux = zero
+  cycle
   aux = aux + (hamil_DD_H2_byT(1,kk) * &
                bogo_UV_operations_for_H22(q1,q2,q3,q4, i1,i2,i3,i4))
   aux = aux + (hamil_DD_H2_byT(2,kk) * ( &
