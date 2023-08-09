235352c99b8bae72b92decdf53a73ac2776e8fd9
migueldelafuente1 <migueldelafuente4@gmail.com>
2023-08-09 21:45:44 +0200

    supress BB term for the hamil

--- src/module_exportings.f90
+++ src/module_exportings.f90
@@ -1728,10 +1728,10 @@ V_trans = bogo_V0
     close(334)
 
 !! Apply the transformation on the U and V
-call dgemm('n','n', ndim, ndim, ndim, one, bogo_U0, ndim, transf_H11, ndim,&
-           zero, U_trans, ndim)
-call dgemm('n','n', ndim, ndim, ndim, one, bogo_V0, ndim, transf_H11, ndim,&
-           zero, V_trans, ndim)
+!call dgemm('n','n', ndim, ndim, ndim, one, bogo_U0, ndim, transf_H11, ndim,&
+!           zero, U_trans, ndim)
+!call dgemm('n','n', ndim, ndim, ndim, one, bogo_V0, ndim, transf_H11, ndim,&
+!           zero, V_trans, ndim)
 
 !call dgemm('t','n', ndim, ndim, ndim, one, transf_H11, ndim, bogo_U0, ndim,&
 !           zero, aux_U, ndim)
