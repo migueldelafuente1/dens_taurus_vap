f03c96bd16f8251ce8a85c7a21ce21896b550244
migueldelafuente1 <migueldelafuente4@gmail.com>
2023-08-16 11:52:59 +0200

    debug

--- src/module_exportings.f90
+++ src/module_exportings.f90
@@ -524,7 +524,7 @@ open(300, file="onlyDD_D1S_scalar.2b")
 open(301, file="onlyDD_D1S_k1.2b")
 open(302, file="onlyDD_D1S_k2.2b")
 do KK = 0, TENSOR_ORD
-  write(300+KK, fmt='(A,I3,A,F10.5,A,F10.5,A,F6.4)') &
+  write(300+KK, fmt="(A,I3,A,F10.5,A,F10.5,A,F6.4)") &
     'Density 2BME on explicit HFB wf from taurus, Tensor=',KK, &
     ' PARAMS:: t3=',t3_DD_CONST,' MeV  X0=', x0_DD_FACTOR, ' ALPHA=', alpha_DD
 enddo
