a3b2e3432e9940ae6e58b37ee921300112987a34
migueldelafuente1 <migueldelafuente4@gmail.com>
2023-08-17 12:49:41 +0200

    modiff for the e sp and E core

--- src/module_exportings.f90
+++ src/module_exportings.f90
@@ -479,7 +479,7 @@ close(297)
 close(298)
 
 deallocate(T_core, V_core, ep_sp_vs, en_sp_vs, t_sp_vs)
-!deallocate(hamil_DDcpd)
+deallocate(hamil_DDcpd)
 
 print *,  " [OK] calculate_valenceSpaceReduced"
 print *, ""
