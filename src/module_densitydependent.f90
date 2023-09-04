fae94805a8a581d74be71d65729c053898fa62e5
migueldelafuente1 <migueldelafuente4@gmail.com>
2023-09-04 11:37:57 +0200

    test for complex numbers

--- src/module_densitydependent.f90
+++ src/module_densitydependent.f90
@@ -548,9 +548,10 @@ subroutine set_densty_dependent(seedtype, itermax, proj_Mphip, proj_Mphin)
     r  = sqrt(dreal(z)**2 + dimag(z)**2)
     a  = acos(dreal(z)/ r)
     a2 = 0.0
-    if (dimag(z) .lt. 0.0) then
-      a2 = ((1.0d0 - (dimag(z)/max(dabs(dimag(z)), 1.0d-13))) / 2) * 2*(pi - a)
-      endif
+!    if (dimag(z) .lt. 0.0) then
+!      a2 = ((1.0d0 - (dimag(z)/max(dabs(dimag(z)), 1.0d-13))) / 2) * 2*(pi - a)
+!      endif
+    a2 = ((1.0d0 - (dimag(z)/(dabs(dimag(z))-1.0d-13))) / 2) * 2*(pi - a)
     a = a + a2
 
     x3 = r ** ALP
