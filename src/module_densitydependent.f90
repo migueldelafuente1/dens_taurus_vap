fd14972d3234a4af1ae3a37bb26604bde5664ef5
migueldelafuente1 <migueldelafuente4@gmail.com>
2023-12-26 17:32:24 +0100

    fixings in angular part for gradient dd interaction

--- src/module_densitydependent.f90
+++ src/module_densitydependent.f90
@@ -3832,8 +3832,9 @@ if (abs(imag(v_dd_value(2))) > 1.0d-9 ) then
 endif
 
 if (dabs(v_dd_val_Real(2)).GT.1.0d-6) then
-  print "(A,4I5,A,2D15.9)", "   _Eval me(pp/pn):",a,b,c,d, "=", &
-                            v_dd_val_Real(1), v_dd_val_Real(2)
+  print "(2(A,4I6),A,2D20.9)", "   _Eval me(pp/pn):",a,b,c,d, " <", &
+                HOsp_ant(a), HOsp_ant(b),HOsp_ant(c),HOsp_ant(d), "> =", &
+                v_dd_val_Real(1), v_dd_val_Real(2)
 endif
 return
 end function matrix_element_v_gradientDD
